05c918e12dd90ef259be8c927dcdfdcdb165bab2
Moshe Katz <moshe@ymkatz.net>
2017-04-25 22:49:27 -0400

    new preprocessing code for multi-level caching

--- bin/preprocess1.php
+++ bin/preprocess1.php
@@ -75,6 +75,145 @@ function addFeature($feature)
 	}
 }
 
+$storagepath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data');
+
+function makeFilenames($depth, $names = ['']) {
+	if ($depth === 0) {
+		return array_map(function ($name) {
+			return 'polygons-' . (strlen($name) === 0 ? 'all' : $name);
+		}, $names);
+	}
+
+	return makeFilenames($depth - 1, array_reduce($names, function ($names, $name) {
+		// Quadrants are: 0 - NW, 1 - NE, 2 - SE, 3 - SW
+		foreach (['0','1','2','3'] as $quadrant) {
+			$names[] = $name . $quadrant;
+		}
+		return $names;
+	}, []));
+}
+
+function getBoundsForFiles($depth, $files = ['' => ['n' => 180, 's' => 0, 'e' => 360, 'w' => 0]]) {
+	if ($depth === 0) {
+		$bounds = [];
+
+		foreach ($files as $name => $poly_bounds) {
+			$poly_bounds['n'] = $poly_bounds['n'] - 90;
+			$poly_bounds['s'] = $poly_bounds['s'] - 90;
+			$poly_bounds['e'] = $poly_bounds['e'] - 180;
+			$poly_bounds['w'] = $poly_bounds['w'] - 180;
+
+			$bounds['polygons-' . (strlen($name) === 0 ? 'all' : $name)] = $poly_bounds;
+		}
+
+		return $bounds;
+	}
+
+	$new_files = [];
+
+	foreach ($files as $name => $parent_bounds) {
+		// Quadrants are: 0 - NW, 1 - NE, 2 - SE, 3 - SW
+		foreach (['0','1','2','3'] as $quadrant) {
+			$full_quad_name = $name . $quadrant;
+			for ($i = 0; $i < strlen($full_quad_name); $i++) {
+				$subquadrant = $full_quad_name[$i];
+				switch ($subquadrant) {
+					case 0:
+						$bounds = [
+							'n' => $parent_bounds['n'],
+							's' => $parent_bounds['s'] + (($parent_bounds['n'] - $parent_bounds['s']) / 2),
+							'e' => $parent_bounds['e'] - (($parent_bounds['e'] - $parent_bounds['w']) / 2),
+							'w' => $parent_bounds['w'],
+						];
+						break;
+					case 1:
+						$bounds = [
+							'n' => $parent_bounds['n'],
+							's' => $parent_bounds['s'] + (($parent_bounds['n'] - $parent_bounds['s']) / 2),
+							'e' => $parent_bounds['e'],
+							'w' => $parent_bounds['w'] + (($parent_bounds['e'] - $parent_bounds['w']) / 2),
+						];
+						break;
+					case 2:
+						$bounds = [
+							'n' => $parent_bounds['n'] - (($parent_bounds['n'] - $parent_bounds['s']) / 2),
+							's' => $parent_bounds['s'],
+							'e' => $parent_bounds['e'],
+							'w' => $parent_bounds['w'] + (($parent_bounds['e'] - $parent_bounds['w']) / 2),
+						];
+						break;
+					case 3:
+						$bounds = [
+							'n' => $parent_bounds['n'] - (($parent_bounds['n'] - $parent_bounds['s']) / 2),
+							's' => $parent_bounds['s'],
+							'e' => $parent_bounds['e'] - (($parent_bounds['e'] - $parent_bounds['w']) / 2),
+							'w' => $parent_bounds['w'],
+						];
+						break;
+				}
+			}
+
+			$new_files[$full_quad_name] = $bounds;
+		}
+	}
+
+	return getBoundsForFiles($depth - 1, $new_files);
+}
+
+function polygonsOverlapWithBox($polys, $box) {
+	foreach ($polys as $poly) {
+		if (
+			$poly->getNorth() >= $box['s'] &&
+			$poly->getSouth() <= $box['n'] &&
+			$poly->getEast()  >= $box['w'] &&
+			$poly->getWest()  <= $box['e']
+		) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Write the data to serialized object file(s) as a quadtree of the depth given
+ *
+ * @param  integer $depth How many times to divide into quadrants when splitting up the results
+ */
+function writeFiles($depth = 0) {
+	global $storagepath, $timezoneNamesToPolygons;
+
+	if ($depth === 0) {
+		file_put_contents($storagepath . DIRECTORY_SEPARATOR . 'polygons', serialize($timezoneNamesToPolygons));
+		return;
+	}
+
+	$files = [];
+	foreach (makeFilenames($depth) as $name) {
+		if (! isset($files[$name])) {
+			$files[$name] = [];
+		}
+	}
+	$file_bounds = getBoundsForFiles($depth);
+
+	file_put_contents($storagepath . DIRECTORY_SEPARATOR . 'polygons-meta', serialize([
+		'depth' => $depth,
+		'file_bounds' => $file_bounds,
+	]));
+
+	foreach ($timezoneNamesToPolygons as $name => $polys) {
+		foreach ($file_bounds as $file => $bounds) {
+			if (polygonsOverlapWithBox($polys, $bounds)) {
+				$files[$file][$name] = $polys;
+			}
+		}
+	}
+
+	foreach ($files as $name => $values) {
+		file_put_contents($storagepath . DIRECTORY_SEPARATOR . $name, serialize($values));
+	}
+}
+
 $time_start = microtime_float();
 try {
 	$shapefile = new ShapeFile($tzWorldFile);
@@ -88,7 +227,7 @@ try {
     $time_end1 = microtime_float();
 	echo "Generated in " . ($time_end1 - $time_start) . "sec\n";
 
-    file_put_contents(realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data') . DIRECTORY_SEPARATOR . 'polygons', serialize($timezoneNamesToPolygons));
+    writeFiles(TzWhere::POLYGON_FILE_SPLIT_DEPTH);
 
     $time_end2 = microtime_float();
 	echo "Saved in " . ($time_end2 - $time_end1) . "sec\n";
